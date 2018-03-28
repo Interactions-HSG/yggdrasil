8476e6718046124bef99eef2dd7f095f8eebde13
Andrei Ciortea <andrei.ciortea@gmail.com>
2018-03-28 09:53:38 +0300

    Add script for registering Hue artifact on a 2nd node

--- scripts/dist-register-hue-artifact.sh
+++ scripts/dist-register-hue-artifact.sh
@@ -2,7 +2,7 @@
 
 echo -e '\n\nCreating hue artifact...'
 curl -i -X POST \
-  http://localhost:8080/artifacts/ \
+  http://localhost:8085/artifacts/ \
   -H 'content-type: text/turtle' \
   -H 'slug: hue1' \
   -d '<> a <http://w3id.org/eve#Artifact> ;
@@ -16,6 +16,6 @@ curl -i -X PUT \
   -H 'content-type: text/turtle' \
   -d '<http://localhost:8080/workspaces/wksp1> a <http://w3id.org/eve#Workspace> ;
 <http://w3id.org/eve#hasName> "wksp1" ;
-<http://w3id.org/eve#contains> <http://localhost:8080/artifacts/hue1> ;
+<http://w3id.org/eve#contains> <http://localhost:8085/artifacts/hue1> ;
 <http://w3id.org/eve#contains> <http://localhost:8080/artifacts/event-gen> .'
 
