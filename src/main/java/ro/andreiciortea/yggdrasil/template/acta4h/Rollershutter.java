43a91b3fe89e82d900a46997375eb53203011421
Simon Bienz <bienzs@student.ethz.ch>
2019-03-27 11:18:32 +0100

    [FXI] rollershuter observable property

--- src/main/java/ro/andreiciortea/yggdrasil/template/acta4h/Rollershutter.java
+++ src/main/java/ro/andreiciortea/yggdrasil/template/acta4h/Rollershutter.java
@@ -2,9 +2,12 @@ package ro.andreiciortea.yggdrasil.template.acta4h;
 
 import ro.andreiciortea.yggdrasil.template.annotation.Action;
 import ro.andreiciortea.yggdrasil.template.annotation.Artifact;
+import ro.andreiciortea.yggdrasil.template.annotation.ObservableProperty;
 
 @Artifact(type = "Rollershutter")
 public class Rollershutter {
+
+  @ObservableProperty
   public double percentage = 0.0;
 
   @Action(path = "/actions/setPercentage")
