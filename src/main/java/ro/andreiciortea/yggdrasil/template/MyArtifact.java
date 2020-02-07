aecfb028c7b215f8eb2b6a1caed9d222353fc0b2
phil9987 <philipjunker@gmail.com>
2020-02-07 13:55:35 +0100

    add type URIs instead of just a name

--- src/main/java/ro/andreiciortea/yggdrasil/template/MyArtifact.java
+++ src/main/java/ro/andreiciortea/yggdrasil/template/MyArtifact.java
@@ -3,7 +3,7 @@ package ro.andreiciortea.yggdrasil.template;
 import ro.andreiciortea.yggdrasil.template.annotation.*;
 
 // TODO: enable multiple types
-@Artifact(type = "Thing", additions =
+@Artifact(type = "http://example.org/#Thing", additions =
   @RdfAddition(predicates ={"eve:test1"}, objects = {"eve:obj1"})
 )
 /*
