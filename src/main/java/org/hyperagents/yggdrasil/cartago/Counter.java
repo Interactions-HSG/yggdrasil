007e9d1d58ed71cb18cfc56bc9fa25385fdf07a9
danaivach <danai.vacht@gmail.com>
2020-12-10 11:34:00 +0100

    Fix increment url

--- src/main/java/org/hyperagents/yggdrasil/cartago/Counter.java
+++ src/main/java/org/hyperagents/yggdrasil/cartago/Counter.java
@@ -4,17 +4,17 @@ import cartago.OPERATION;
 import cartago.ObsProperty;
 
 public class Counter extends HypermediaArtifact {
-  
+
   public void init() {
     defineObsProperty("count", 0);
   }
-  
+
   @OPERATION
   public void inc() {
     ObsProperty prop = getObsProperty("count");
     prop.updateValue(prop.intValue() + 1);
   }
-  
+
   @Override
   protected void registerInteractionAffordances() {
     // Register one action affordance with an input schema
