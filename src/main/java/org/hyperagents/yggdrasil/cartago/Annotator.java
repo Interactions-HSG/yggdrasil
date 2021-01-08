be563de35255a0a93b16e78e7f8c6a5ea35c27e6
danaivach <danai.vacht@gmail.com>
2021-01-08 11:43:11 +0100

    Create annotator

--- src/main/java/org/hyperagents/yggdrasil/cartago/Annotator.java
+++ src/main/java/org/hyperagents/yggdrasil/cartago/Annotator.java
@@ -1,4 +1,32 @@
 package org.hyperagents.yggdrasil.cartago;
 
-public class Annotator {
+import cartago.OPERATION;
+import ch.unisg.ics.interactions.wot.td.schemas.ObjectSchema;
+import ch.unisg.ics.interactions.wot.td.schemas.StringSchema;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+public class Annotator extends HypermediaArtifact{
+  private static final String PREFIX = "http://example.org/";
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(Annotator.class.getName());
+
+  @OPERATION
+  public void annotate(String uri, String text) {
+    LOGGER.info("Annotate " + uri + " with text " + text);
+  }
+
+  @Override
+  protected void registerInteractionAffordances() {
+    registerActionAffordance(PREFIX +"CreateAnnotation", "annotate", "/annotations",
+      new ObjectSchema.Builder()
+        .addProperty("uri", new StringSchema.Builder()
+          .addSemanticType(PREFIX + "Uri")
+          .build())
+        .addProperty("text", new StringSchema.Builder()
+          .addSemanticType(PREFIX + "Text")
+          .build())
+        .addRequiredProperties("uri", "text")
+        .build());
+  }
 }
