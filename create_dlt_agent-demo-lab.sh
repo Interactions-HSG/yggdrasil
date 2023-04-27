06d8fa29144961daa0e6a48ac42bd1fb369effed
Andreas Ziller <andreas.ziller@siemens.com>
2023-04-27 16:05:24 +0200

    added lab scripts

---
+++ create_dlt_agent-demo-lab.sh
@@ -0,0 +1,1 @@
+./create_dlt_agent.sh --hyper edge.fritz.box:8888 --agent manager
--- initialize-demo-lab.sh
+++ initialize-demo-lab.sh
@@ -1,1 +1,1 @@
-./initialization.sh --hyper edge.fritz.box:8888 --device micro1.fritz.box
+./initialization.sh --hyper edge.fritz.box:8888 --device micro1.fritz.box --dlt edge.fritz.box:9090
