cfd3846a9b9cf3007f78950f9a2d059500b98395
Matteo Castellucci <matteo.castellucci@outlook.com>
2023-12-07 11:02:27 +0100

    test: simplify KnowledgeGraph artifact test

--- src/test/java/org/hyperagents/yggdrasil/KnowledgeGraphArtifactTest.java
+++ src/test/java/org/hyperagents/yggdrasil/KnowledgeGraphArtifactTest.java
@@ -6,122 +6,108 @@ import cartago.CartagoException;
 import cartago.ICartagoContext;
 import cartago.Op;
 import cartago.OpFeedbackParam;
-import cartago.Workspace;
 import cartago.events.ActionFailedEvent;
 import cartago.events.ActionSucceededEvent;
 import cartago.utils.BasicLogger;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
-import io.vertx.ext.web.client.WebClient;
+import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.channels.SocketChannel;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.AfterAll;
+import org.hyperagents.yggdrasil.cartago.CartagoVerticle;
+import org.hyperagents.yggdrasil.eventbus.messageboxes.HttpNotificationDispatcherMessagebox;
+import org.hyperagents.yggdrasil.eventbus.messageboxes.RdfStoreMessagebox;
+import org.hyperagents.yggdrasil.eventbus.messages.RdfStoreMessage;
+import org.hyperagents.yggdrasil.store.RdfStoreVerticle;
+import org.hyperagents.yggdrasil.utils.impl.HttpInterfaceConfigImpl;
+import org.hyperagents.yggdrasil.utils.impl.RepresentationFactoryImpl;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
 @ExtendWith(VertxExtension.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class KnowledgeGraphArtifactTest {
   private static final String TEST_AGENT_NAME = "test";
-  private static final String TEST_HOST = "localhost";
-  private static final int TEST_PORT = 3000;
-  private static final int CARTAGO_PORT = 3001;
   private static final String RESULTS_EQUAL_MESSAGE = "The results should be equal";
   private static final String KNOWLEDGE_GRAPH_ARTIFACT_NAME = "knowledge_graph";
+  private static final String TEST_WORKSPACE_NAME = "test";
 
-  private final Process yggdrasilProcess;
   private final List<Promise<Object>> completionPromises;
   private final ICartagoContext yggdrasilWorkspace;
-  private int completionsCount;
 
   @SuppressFBWarnings("CT_CONSTRUCTOR_THROW")
-  public KnowledgeGraphArtifactTest() throws IOException, CartagoException {
+  public KnowledgeGraphArtifactTest() throws CartagoException {
     this.completionPromises = Stream.generate(Promise::promise)
                                     .limit(5)
                                     .toList();
-    this.completionsCount = 0;
-    final var processBuilder = new ProcessBuilder();
-    processBuilder.environment().put("VERTX_CONFIG_PATH", "src/test/resources/config.json");
-    this.yggdrasilProcess = processBuilder.command("./gradlew", "run", "--no-daemon")
-                                          .inheritIO()
-                                          .start();
-    var started = false;
-    do {
-      try (var ignored = SocketChannel.open(new InetSocketAddress(TEST_HOST, CARTAGO_PORT))) {
-        started = true;
-      } catch (final IOException ignored) { }
-    } while (!started);
     final var environment = CartagoEnvironment.getInstance();
     environment.init(new BasicLogger());
-    environment.installInfrastructureLayer("web");
-    this.yggdrasilWorkspace = environment.joinRemoteWorkspace(
-      "",
-      TEST_HOST + ":" + CARTAGO_PORT,
-      "/main",
-      "web",
+    this.yggdrasilWorkspace = environment.getRootWSP().getWorkspace().joinWorkspace(
       new AgentIdCredential(TEST_AGENT_NAME),
       e -> {
-        if (e instanceof ActionSucceededEvent s) {
+        if (
+            e instanceof ActionSucceededEvent s
+            && Optional.ofNullable(s.getOp())
+                       .map(Op::getName)
+                       .map(Set.of("ask", "select", "selectOne")::contains)
+                       .orElse(false)
+        ) {
           this.completionPromises
-              .get(this.completionsCount)
+              .get((int) s.getActionId())
               .complete(((OpFeedbackParam<?>) s.getOp().getParamValues()[1]).get());
         } else if (e instanceof ActionFailedEvent f) {
           this.completionPromises
-              .get(this.completionsCount)
+              .get((int) f.getActionId())
               .fail(f.getFailureMsg());
         }
-        this.completionsCount++;
-      },
-      "yggdrasil"
+      }
     );
   }
 
-  @BeforeAll
-  public void beforeAll(final Vertx vertx, final VertxTestContext ctx) {
-    final WebClient client = WebClient.create(vertx);
-    client.post(TEST_PORT, TEST_HOST, "/workspaces/")
-          .putHeader("Slug", "test")
-          .putHeader("X-Agent-WebID", TEST_AGENT_NAME)
-          .send()
-          .compose(r -> client.post(TEST_PORT, TEST_HOST, "/workspaces/test")
-                              .putHeader("Slug", "sub")
-                              .putHeader("X-Agent-WebID", TEST_AGENT_NAME)
-                              .send())
-          .onComplete(ctx.succeedingThenComplete());
+  @BeforeEach
+  public void setUp(final Vertx vertx, final VertxTestContext ctx) {
+    final var messagebox = new RdfStoreMessagebox(vertx.eventBus());
+    final var httpConfig = new HttpInterfaceConfigImpl(JsonObject.of());
+    final var representationFactory = new RepresentationFactoryImpl(httpConfig);
+    final var notificationMessagebox = new HttpNotificationDispatcherMessagebox(vertx.eventBus());
+    notificationMessagebox.init();
+    vertx.deployVerticle(new CartagoVerticle())
+         .compose(i -> vertx.deployVerticle(new RdfStoreVerticle()))
+         .compose(i -> messagebox.sendMessage(new RdfStoreMessage.CreateWorkspace(
+           httpConfig.getWorkspacesUri() + "/",
+           TEST_WORKSPACE_NAME,
+           Optional.empty(),
+           representationFactory.createWorkspaceRepresentation(TEST_WORKSPACE_NAME, Set.of())
+         )))
+         .compose(r -> messagebox.sendMessage(new RdfStoreMessage.CreateWorkspace(
+           httpConfig.getWorkspacesUri() + "/",
+           "sub",
+           Optional.of(httpConfig.getWorkspaceUri(TEST_WORKSPACE_NAME)),
+           representationFactory.createWorkspaceRepresentation("sub", Set.of())
+         )))
+         .onComplete(ctx.succeedingThenComplete());
   }
 
-  @AfterAll
-  public void afterAll(final Vertx vertx, final VertxTestContext ctx) throws CartagoException {
-    CartagoEnvironment.getInstance().shutdown();
-    // Resetting CArtAgO root workspace before shutting down to ensure system is fully reset
-    final var rootWorkspace = CartagoEnvironment.getInstance().getRootWSP();
-    rootWorkspace.setWorkspace(new Workspace(
-        rootWorkspace.getId(),
-        rootWorkspace,
-        new BasicLogger()
-    ));
-    this.yggdrasilProcess.onExit().thenRun(() -> vertx.close(ctx.succeedingThenComplete()));
-    this.yggdrasilProcess.destroy();
+  @AfterEach
+  public void tearDown(final Vertx vertx, final VertxTestContext ctx) {
+    vertx.close(ctx.succeedingThenComplete());
   }
 
   @Test
   public void testAskQuery(final VertxTestContext ctx) throws CartagoException {
     this.completionPromises
-        .get(this.completionsCount)
+        .get(0)
         .future()
-        .onSuccess(r -> Assertions.assertEquals(
-          true,
-          r,
+        .onSuccess(r -> Assertions.assertTrue(
+          (Boolean) r,
           RESULTS_EQUAL_MESSAGE
         ))
         .onComplete(ctx.succeedingThenComplete());
@@ -151,17 +137,17 @@ public class KnowledgeGraphArtifactTest {
   @Test
   public void testSelectQuery(final VertxTestContext ctx) throws CartagoException {
     this.completionPromises
-        .get(this.completionsCount)
+        .get(1)
         .future()
         .onSuccess(r -> Assertions.assertArrayEquals(
           new String[][][] {
             new String[][]{
-              new String[]{"name", "test"},
-              new String[]{"uri", "http://localhost:3000/workspaces/test"}
+              new String[]{"name", TEST_WORKSPACE_NAME},
+              new String[]{"uri", "http://localhost:8080/workspaces/test"}
             },
             new String[][]{
               new String[]{"name", "sub"},
-              new String[]{"uri", "http://localhost:3000/workspaces/sub"}
+              new String[]{"uri", "http://localhost:8080/workspaces/sub"}
             }
           },
           (String[][][]) r,
@@ -169,7 +155,7 @@ public class KnowledgeGraphArtifactTest {
         ))
         .onComplete(ctx.succeedingThenComplete());
     this.yggdrasilWorkspace
-        .doAction(0,
+        .doAction(1,
                   KNOWLEDGE_GRAPH_ARTIFACT_NAME,
                   new Op(
                     "select",
@@ -192,7 +178,7 @@ public class KnowledgeGraphArtifactTest {
   @Test
   public void testSelectQueryWithNoResult(final VertxTestContext ctx) throws CartagoException {
     this.completionPromises
-        .get(this.completionsCount)
+        .get(2)
         .future()
         .onSuccess(r -> Assertions.assertArrayEquals(
           new String[][][] {},
@@ -201,7 +187,7 @@ public class KnowledgeGraphArtifactTest {
         ))
         .onComplete(ctx.succeedingThenComplete());
     this.yggdrasilWorkspace
-        .doAction(0,
+        .doAction(2,
                   KNOWLEDGE_GRAPH_ARTIFACT_NAME,
                   new Op(
                     "select",
@@ -224,18 +210,18 @@ public class KnowledgeGraphArtifactTest {
   @Test
   public void testSelectOneQuery(final VertxTestContext ctx) throws CartagoException {
     this.completionPromises
-        .get(this.completionsCount)
+        .get(3)
         .future()
         .onSuccess(r -> Assertions.assertArrayEquals(
           new String[][]{
-            new String[]{"uri", "http://localhost:3000/workspaces/sub"}
+            new String[]{"uri", "http://localhost:8080/workspaces/sub"}
           },
           (String[][]) r,
           RESULTS_EQUAL_MESSAGE
         ))
         .onComplete(ctx.succeedingThenComplete());
     this.yggdrasilWorkspace
-        .doAction(0,
+        .doAction(3,
                   KNOWLEDGE_GRAPH_ARTIFACT_NAME,
                   new Op(
                     "selectOne",
@@ -258,7 +244,7 @@ public class KnowledgeGraphArtifactTest {
   @Test
   public void testSelectOneQueryWithNoResult(final VertxTestContext ctx) throws CartagoException {
     this.completionPromises
-        .get(this.completionsCount)
+        .get(4)
         .future()
         .onSuccess(r -> Assertions.assertNull(
           r,
@@ -266,7 +252,7 @@ public class KnowledgeGraphArtifactTest {
         ))
         .onComplete(ctx.succeedingThenComplete());
     this.yggdrasilWorkspace
-        .doAction(0,
+        .doAction(4,
                   KNOWLEDGE_GRAPH_ARTIFACT_NAME,
                   new Op(
                     "selectOne",
