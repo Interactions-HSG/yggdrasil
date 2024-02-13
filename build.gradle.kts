1122aff7d18ece7f0f9a38e955f4c9d9864cb9ed
KaiTries <112092835+KaiTries@users.noreply.github.com>
2024-02-13 18:36:10 +0100

    dependencies added
    Added necessary dependencies for hmas-java

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,6 +53,19 @@ dependencies {
   implementation(project(":yggdrasil-cartago"))
   implementation(project(":yggdrasil-websub"))
 
+
+  implementation(files("libs/bindings-1.0-SNAPSHOT.jar"))
+  implementation(files("libs/core-1.0-SNAPSHOT.jar"))
+  implementation(files("libs/interaction-1.0-SNAPSHOT.jar"))
+  implementation("org.eclipse.rdf4j:rdf4j-runtime:3.7.4@pom") {
+    isTransitive = true
+  }
+  implementation("com.google.guava:guava:11.0.2")
+  implementation("io.vavr:vavr:0.10.4")
+  implementation("com.google.code.gson:gson:2.10.1")
+  implementation("org.apache.httpcomponents.client5:httpclient5:5.2.2")
+  implementation("org.apache.httpcomponents.core5:httpcore5:5.2.2")
+
   implementation(libs.vertx.core)
   implementation(libs.vertx.config)
 
