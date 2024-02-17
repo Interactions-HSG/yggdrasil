34a3c62cce34e0f74dcd86a29f427a5deccfbd5a
KaiTries <112092835+KaiTries@users.noreply.github.com>
2024-02-17 19:21:42 +0100

    added javadocs

--- yggdrasil-utils/src/main/java/org/hyperagents/yggdrasil/model/Environment.java
+++ yggdrasil-utils/src/main/java/org/hyperagents/yggdrasil/model/Environment.java
@@ -5,7 +5,14 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * TODO: Javadoc.
+ * An interface representing an Artifact in the Yggdrasil model.
+ *
+ * <p>An Artifact is a component of the Yggdrasil model that has a name, a class, initialization
+ * parameters,
+ * a set of focusing agents, and a representation.
+ *
+ * <p>The class, initialization parameters, and representation are optional and may not be
+ * present for all artifacts.
  */
 public interface Environment extends Shareable {
   List<Workspace> getWorkspaces();
--- yggdrasil-utils/src/main/java/org/hyperagents/yggdrasil/model/Workspace.java
+++ yggdrasil-utils/src/main/java/org/hyperagents/yggdrasil/model/Workspace.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import java.util.Set;
 
 /**
- * TODO: Javadoc.
+ * An interface representing a Joined Agent in the Yggdrasil model.
+ *
+ * <p>A Joined Agent is a type of agent that has joined a workspace in the Yggdrasil model.
+ * Each Joined Agent has a unique name.
  */
 public interface Workspace {
   String getName();
