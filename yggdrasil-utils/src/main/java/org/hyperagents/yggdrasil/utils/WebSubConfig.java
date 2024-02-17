65c6a295017f6b12e668a0779c64b810d396a3d3
KaiTries <112092835+KaiTries@users.noreply.github.com>
2024-02-17 19:56:23 +0100

    added javacods

--- yggdrasil-utils/src/main/java/org/hyperagents/yggdrasil/utils/WebSubConfig.java
+++ yggdrasil-utils/src/main/java/org/hyperagents/yggdrasil/utils/WebSubConfig.java
@@ -3,10 +3,21 @@ package org.hyperagents.yggdrasil.utils;
 import io.vertx.core.shareddata.Shareable;
 
 /**
- * TODO: Javadoc.
+ * Represents the configuration for WebSub.
  */
 public interface WebSubConfig extends Shareable {
+  
+  /**
+   * Checks if WebSub is enabled.
+   *
+   * @return true if WebSub is enabled, false otherwise.
+   */
   boolean isEnabled();
 
+  /**
+   * Gets the WebSub hub URI.
+   *
+   * @return the WebSub hub URI.
+   */
   String getWebSubHubUri();
 }
