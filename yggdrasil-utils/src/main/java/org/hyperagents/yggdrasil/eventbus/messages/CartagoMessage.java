246ee67f701d9e90e469146291efece2d019fb49
KaiTries <112092835+KaiTries@users.noreply.github.com>
2024-02-17 19:14:56 +0100

    added javadocs

--- yggdrasil-utils/src/main/java/org/hyperagents/yggdrasil/eventbus/messages/CartagoMessage.java
+++ yggdrasil-utils/src/main/java/org/hyperagents/yggdrasil/eventbus/messages/CartagoMessage.java
@@ -3,53 +3,112 @@ package org.hyperagents.yggdrasil.eventbus.messages;
 import java.util.Optional;
 
 /**
- * TODO: Javadoc.
+ * Classifies all messages related to Cartago.
  */
 public sealed interface CartagoMessage {
   String workspaceName();
 
   /**
-   * TODO: Javadoc.
+   * Message used to create a Workspace.
+   *
+   * @param workspaceName The name of the workspace to be created.
    */
-  record CreateWorkspace(String workspaceName) implements CartagoMessage {}
+  record CreateWorkspace(String workspaceName) implements CartagoMessage {
+  }
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to create a subworkspace in a specified workspace in Cartago.
+   *
+   * <p>This record is used if an agent wants to create a subworkspace within a specific workspace.
+   * The workspace is identified by its workspaceName and the subworkspace by its subWorkspaceName.
+   *
+   * @param workspaceName    The name of the workspace where the subworkspace will be created.
+   * @param subWorkspaceName The name of the subworkspace to be created.
    */
-  record CreateSubWorkspace(String workspaceName, String subWorkspaceName)
-      implements CartagoMessage {}
+  record CreateSubWorkspace(String workspaceName,
+                            String subWorkspaceName) implements CartagoMessage {
+  }
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to join a workspace in Cartago.
+   *
+   * <p>This record is used when an agent wants to join a specific workspace.
+   * The agent is identified by its agentId and the workspace by its workspaceName.
+   *
+   * @param agentId       The unique identifier of the agent that wants to join the workspace.
+   * @param workspaceName The name of the workspace the agent wants to join.
    */
-  record JoinWorkspace(String agentId, String workspaceName) implements CartagoMessage {}
+  record JoinWorkspace(String agentId, String workspaceName) implements CartagoMessage {
+  }
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to leave a workspace in Cartago.
+   *
+   * <p>This record is used when an agent wants to leave a specific workspace.
+   * The agent is identified by its agentId and the workspace by its workspaceName.
+   *
+   * @param agentId       The unique identifier of the agent that wants to leave the workspace.
+   * @param workspaceName The name of the workspace the agent wants to leave.
    */
-  record LeaveWorkspace(String agentId, String workspaceName) implements CartagoMessage {}
+  record LeaveWorkspace(String agentId, String workspaceName) implements CartagoMessage {
+  }
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to focus on a specific artifact in a workspace in Cartago.
+   *
+   * <p>This record is used when an agent wants to focus on a specific artifact within a specific
+   * workspace.
+   * The agent is identified by its agentId, the workspace by its workspaceName, and the artifact
+   * by its artifactName.
+   *
+   * @param agentId       The unique identifier of the agent that wants to focus on the artifact.
+   * @param workspaceName The name of the workspace where the artifact is located.
+   * @param artifactName  The name of the artifact the agent wants to focus on.
    */
   record Focus(
       String agentId,
       String workspaceName,
       String artifactName
-  ) implements CartagoMessage {}
+  ) implements CartagoMessage {
+  }
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to create an artifact in a workspace in Cartago.
+   *
+   * <p>This record is used when an agent wants to create a specific artifact within a specific
+   * workspace.
+   * The agent is identified by its agentId, the workspace by its workspaceName, and the artifact
+   * by its artifactName.
+   * The representation parameter is a string that represents the state of the artifact.
+   *
+   * @param agentId        The unique identifier of the agent that wants to create the artifact.
+   * @param workspaceName  The name of the workspace where the artifact will be created.
+   * @param artifactName   The name of the artifact to be created.
+   * @param representation The string representation of the artifact's state.
    */
   record CreateArtifact(
       String agentId,
       String workspaceName,
       String artifactName,
       String representation
-  ) implements CartagoMessage {}
+  ) implements CartagoMessage {
+  }
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to perform an action on an artifact in a workspace in Cartago.
+   *
+   * <p>This record is used when an agent wants to perform a specific action on a
+   * specific artifact within a specific workspace.
+   * The agent is identified by its agentId, the workspace by its workspaceName,
+   * the artifact by its artifactName, and the action by its actionName.
+   * The content parameter is an optional string that may contain additional information for the
+   * action.
+   *
+   * @param agentId       The unique identifier of the agent that wants to perform the action.
+   * @param workspaceName The name of the workspace where the artifact is located.
+   * @param artifactName  The name of the artifact on which the action will be performed.
+   * @param actionName    The name of the action to be performed.
+   * @param content       Optional additional information for the action.
    */
   record DoAction(
       String agentId,
@@ -57,5 +116,6 @@ public sealed interface CartagoMessage {
       String artifactName,
       String actionName,
       Optional<String> content
-  ) implements CartagoMessage {}
+  ) implements CartagoMessage {
+  }
 }
--- yggdrasil-utils/src/main/java/org/hyperagents/yggdrasil/eventbus/messages/RdfStoreMessage.java
+++ yggdrasil-utils/src/main/java/org/hyperagents/yggdrasil/eventbus/messages/RdfStoreMessage.java
@@ -5,55 +5,90 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * TODO: Javadoc.
+ * An interface representing a message for RDF store operations.
+ *
+ * <p>This interface is used to define different types of messages that can be sent to perform
+ * operations on an RDF store.
+ * Each record implementing this interface represents a specific operation.
  */
 public sealed interface RdfStoreMessage {
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to get an entity from the RDF store.
+   *
+   * @param requestUri The URI of the request to get the entity.
    */
-  record GetEntity(String requestUri) implements RdfStoreMessage {}
+  record GetEntity(String requestUri) implements RdfStoreMessage {
+  }
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to update an entity in the RDF store.
+   *
+   * @param requestUri           The URI of the request to update the entity.
+   * @param entityRepresentation The string representation of the entity to be updated.
    */
-  record UpdateEntity(String requestUri, String entityRepresentation) implements RdfStoreMessage {}
+  record UpdateEntity(String requestUri, String entityRepresentation) implements RdfStoreMessage {
+  }
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to delete an entity from the RDF store.
+   *
+   * @param requestUri The URI of the request to delete the entity.
    */
-  record DeleteEntity(String requestUri) implements RdfStoreMessage {}
+  record DeleteEntity(String requestUri) implements RdfStoreMessage {
+  }
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to create a body in the RDF store.
+   *
+   * @param workspaceName      The name of the workspace where the body will be created.
+   * @param agentName          The name of the agent for which the body will be created.
+   * @param bodyRepresentation The string representation of the body to be created.
    */
   record CreateBody(
       String workspaceName,
       String agentName,
       String bodyRepresentation
-  ) implements RdfStoreMessage {}
+  ) implements RdfStoreMessage {
+  }
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to create an artifact in the RDF store.
+   *
+   * @param requestUri             The URI of the request to create the artifact.
+   * @param artifactName           The name of the artifact to be created.
+   * @param artifactRepresentation The string representation of the artifact to be created.
    */
   record CreateArtifact(
       String requestUri,
       String artifactName,
       String artifactRepresentation
-  ) implements RdfStoreMessage {}
+  ) implements RdfStoreMessage {
+  }
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to create a workspace in the RDF store.
+   *
+   * @param requestUri              The URI of the request to create the workspace.
+   * @param workspaceName           The name of the workspace to be created.
+   * @param parentWorkspaceUri      The URI of the parent workspace, if any.
+   * @param workspaceRepresentation The string representation of the workspace to be created.
    */
   record CreateWorkspace(
       String requestUri,
       String workspaceName,
       Optional<String> parentWorkspaceUri,
       String workspaceRepresentation
-  ) implements RdfStoreMessage {}
+  ) implements RdfStoreMessage {
+  }
 
   /**
-   * TODO: Javadoc.
+   * A record representing a request to query the knowledge graph in the RDF store.
+   *
+   * @param query               The SPARQL query to be executed.
+   * @param defaultGraphUris    The URIs of the default graphs to be used in the query.
+   * @param namedGraphUris      The URIs of the named graphs to be used in the query.
+   * @param responseContentType The content type of the response.
    */
   record QueryKnowledgeGraph(
       String query,
@@ -62,7 +97,12 @@ public sealed interface RdfStoreMessage {
       String responseContentType
   ) implements RdfStoreMessage {
     /**
-     * TODO: Javadoc.
+     * Constructor for the QueryKnowledgeGraph record.
+     *
+     * @param query               The SPARQL query to be executed.
+     * @param defaultGraphUris    The URIs of the default graphs to be used in the query.
+     * @param namedGraphUris      The URIs of the named graphs to be used in the query.
+     * @param responseContentType The content type of the response.
      */
     public QueryKnowledgeGraph(
         final String query,
@@ -76,11 +116,21 @@ public sealed interface RdfStoreMessage {
       this.responseContentType = responseContentType;
     }
 
+    /**
+     * Getter for the defaultGraphUris field.
+     *
+     * @return A new list containing the URIs of the default graphs.
+     */
     @Override
     public List<String> defaultGraphUris() {
       return new ArrayList<>(this.defaultGraphUris);
     }
 
+    /**
+     * Getter for the namedGraphUris field.
+     *
+     * @return A new list containing the URIs of the named graphs.
+     */
     @Override
     public List<String> namedGraphUris() {
       return new ArrayList<>(this.namedGraphUris);
