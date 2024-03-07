f8351c4aa03ef0bda94cbb5c2783d77481ff9bc4
KaiTries <112092835+KaiTries@users.noreply.github.com>
2024-03-07 16:26:43 +0100

    everything is now with trailing backslash

--- yggdrasil-utils/src/main/java/org/hyperagents/yggdrasil/utils/impl/HttpInterfaceConfigImpl.java
+++ yggdrasil-utils/src/main/java/org/hyperagents/yggdrasil/utils/impl/HttpInterfaceConfigImpl.java
@@ -7,7 +7,7 @@ import org.hyperagents.yggdrasil.utils.HttpInterfaceConfig;
 import org.hyperagents.yggdrasil.utils.JsonObjectUtils;
 
 /**
- * Implementation of the HttpInterfaceConfig interface 
+ * Implementation of the HttpInterfaceConfig interface
  * that provides configuration for an HTTP interface.
  */
 @SuppressWarnings("PMD.AvoidUsingHardCodedIP")
@@ -30,14 +30,9 @@ public class HttpInterfaceConfigImpl implements HttpInterfaceConfig {
     this.port = httpConfig.flatMap(c -> JsonObjectUtils.getInteger(c, "port", LOGGER::error))
                           .orElse(8080);
     this.baseUri =
-      httpConfig
-        .flatMap(c -> JsonObjectUtils.getString(c, "base-uri", LOGGER::error))
-        // Strip away the trailing slash (if any)
-        .map(u -> u.endsWith("/") ? u.substring(0, u.length() - 1) : u)
-        .orElseGet(() -> "http://"
-                         + (this.host.equals("0.0.0.0") ? "localhost" : this.host)
-                         + ":"
-                         + this.port);
+            httpConfig
+                    .flatMap(c -> JsonObjectUtils.getString(c, "base-uri", LOGGER::error)).orElseGet(()
+                -> "http://" + (this.host.equals("0.0.0.0") ? "localhost" : this.host) + ":" + this.port + "/");
   }
 
   @Override
@@ -57,36 +52,36 @@ public class HttpInterfaceConfigImpl implements HttpInterfaceConfig {
 
   @Override
   public String getWorkspacesUri() {
-    return this.baseUri + "/workspaces";
+    return this.baseUri + "workspaces/";
   }
 
   @Override
   public String getWorkspaceUri(final String workspaceName) {
-    return this.getWorkspacesUri() + "/" + workspaceName;
+    return this.getWorkspacesUri() + workspaceName + "/";
   }
 
   @Override
   public String getArtifactsUri(final String workspaceName) {
-    return this.getWorkspaceUri(workspaceName) + "/artifacts";
+    return this.getWorkspaceUri(workspaceName) + "artifacts/";
   }
 
   @Override
   public String getArtifactUri(final String workspaceName, final String artifactName) {
-    return this.getArtifactsUri(workspaceName) + "/" + artifactName;
+    return this.getArtifactsUri(workspaceName) + artifactName + "/";
   }
 
   @Override
   public String getAgentBodiesUri(final String workspaceName) {
-    return this.getWorkspaceUri(workspaceName) + "/agents";
+    return this.getWorkspaceUri(workspaceName) + "agents/";
   }
 
   @Override
   public String getAgentBodyUri(final String workspaceName, final String agentName) {
-    return this.getAgentBodiesUri(workspaceName) + "/" + agentName;
+    return this.getAgentBodiesUri(workspaceName) + agentName + "/";
   }
 
   @Override
   public String getAgentUri(final String agentName) {
-    return this.baseUri + "/agents/" + agentName;
+    return this.baseUri + "/agents/" + agentName + "/";
   }
 }
