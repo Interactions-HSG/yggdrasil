87b3a2bf0e93f3e11deea905741a61b3dd153616
KaiTries <112092835+KaiTries@users.noreply.github.com>
2024-03-17 19:46:17 +0100

    create base Artifact to enable creating artifacts through turtle but still have all functionality

---
+++ yggdrasil-cartago/src/main/java/org/hyperagents/yggdrasil/cartago/artifacts/BasicArtifact.java
@@ -0,0 +1,10 @@
+package org.hyperagents.yggdrasil.cartago.artifacts;
+
+
+@SuppressWarnings("unused")
+public class BasicArtifact extends HypermediaArtifact {
+  @Override
+  protected void registerInteractionAffordances() {
+    // No affordances to register
+  }
+}
