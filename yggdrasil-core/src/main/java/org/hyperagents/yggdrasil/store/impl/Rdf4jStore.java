c490edfa625cfabaab951f00b02bb5c9c92815ef
KaiTries <kai.schultz@bluewin.ch>
2024-05-04 18:46:55 +0200

    clean: removed print statements

--- yggdrasil-core/src/main/java/org/hyperagents/yggdrasil/store/impl/Rdf4jStore.java
+++ yggdrasil-core/src/main/java/org/hyperagents/yggdrasil/store/impl/Rdf4jStore.java
@@ -71,9 +71,6 @@ public class Rdf4jStore implements RdfStore {
     String fixedIri = entityIriString.endsWith("/") ? entityIriString : entityIriString + "/";
     var fixedEntityIri = RdfModelUtils.createIri(fixedIri);
 
-    System.out.println("Getting Entity iri: " + fixedEntityIri.toString());
-
-
     try {
       final Model model = QueryResults.asModel(this.connection.getStatements(null, null, null, fixedEntityIri));
       var connectionNamespaces = new HashMap<String, Namespace>();
@@ -102,8 +99,6 @@ public class Rdf4jStore implements RdfStore {
     String entityIriString = entityIri.toString();
     String fixedIri = entityIriString.endsWith("/") ? entityIriString : entityIriString + "/";
     var fixedEntityIri = RdfModelUtils.createIri(fixedIri);
-
-    System.out.println("Adding Entity iri: " + fixedEntityIri.toString());
 
     try {
       this.connection.add(entityModel, fixedEntityIri);
